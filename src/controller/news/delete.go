8f5916cef97957f31b1c601a7e57ac187934b3a1
axetroy <troy450409405@gmail.com>
2019-01-22 16:26:54 +0800

    refactor: refactor test

--- src/controller/news/delete.go
+++ src/controller/news/delete.go
@@ -1,36 +1,9 @@
 package news
 
 import (
-	"fmt"
 	"github.com/axetroy/go-server/src/service"
-	"github.com/jinzhu/gorm"
 )
 
-func DeleteByField(field, value string) {
-	var (
-		err error
-		tx  *gorm.DB
-	)
-
-	defer func() {
-		if tx != nil {
-			if err != nil {
-				_ = tx.Rollback()
-			} else {
-				_ = tx.Commit()
-			}
-		}
-	}()
-
-	tx = service.Db.Begin()
-
-	raw := fmt.Sprintf("DELETE FROM \"%v\" WHERE %s = '%v'", "news", field, value)
-
-	if err = tx.Exec(raw).Error; err != nil {
-		return
-	}
-}
-
 func DeleteNewsById(id string) {
-	DeleteByField("id", id)
+	service.DeleteRowByTable("news", "id", id)
 }
