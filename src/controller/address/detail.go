3c58c2d046f9d91c1ed4f947147f1a951c429dc8
axetroy <troy450409405@gmail.com>
2019-05-10 11:38:11 +0800

    feat: 获取收货地址详情

---
+++ src/controller/address/detail.go
@@ -0,0 +1,85 @@
+package address
+
+import (
+	"errors"
+	"github.com/axetroy/go-server/src/controller"
+	"github.com/axetroy/go-server/src/exception"
+	"github.com/axetroy/go-server/src/model"
+	"github.com/axetroy/go-server/src/schema"
+	"github.com/axetroy/go-server/src/service"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/mitchellh/mapstructure"
+	"net/http"
+	"time"
+)
+
+func GetDetail(context controller.Context, id string) (res schema.Response) {
+	var (
+		err  error
+		data = schema.Address{}
+	)
+
+	defer func() {
+		if r := recover(); r != nil {
+			switch t := r.(type) {
+			case string:
+				err = errors.New(t)
+			case error:
+				err = t
+			default:
+				err = exception.Unknown
+			}
+		}
+
+		if err != nil {
+			res.Data = nil
+			res.Message = err.Error()
+		} else {
+			res.Data = data
+			res.Status = schema.StatusSuccess
+		}
+	}()
+
+	addressInfo := model.Address{
+		Id:  id,
+		Uid: context.Uid,
+	}
+
+	if err = service.Db.First(&addressInfo).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			err = exception.NewsNotExist
+		}
+		return
+	}
+
+	if err = mapstructure.Decode(addressInfo, &data.AddressPure); err != nil {
+		return
+	}
+
+	data.CreatedAt = addressInfo.CreatedAt.Format(time.RFC3339Nano)
+	data.UpdatedAt = addressInfo.UpdatedAt.Format(time.RFC3339Nano)
+
+	return
+}
+
+func GetDetailRouter(context *gin.Context) {
+	var (
+		err error
+		res = schema.Response{}
+	)
+
+	defer func() {
+		if err != nil {
+			res.Data = nil
+			res.Message = err.Error()
+		}
+		context.JSON(http.StatusOK, res)
+	}()
+
+	id := context.Param("address_id")
+
+	res = GetDetail(controller.Context{
+		Uid: context.GetString("uid"),
+	}, id)
+}
--- src/controller/context.go
+++ src/controller/context.go
@@ -2,7 +2,7 @@ package controller
 
 // 控制器的上下文
 type Context struct {
-	Uid       string `json:"uid"`        // 操作人
+	Uid       string `json:"uid"`        // 操作人的用户 ID
 	UserAgent string `json:"user_agent"` // 用户代理
 	Ip        string `json:"ip"`         // IP地址
 }
