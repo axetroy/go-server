692b93711a75e79d3638e55d05f08045722d75d2
axetroy <troy450409405@gmail.com>
2019-05-10 23:22:38 +0800

    test: 添加测试用例

--- src/app_test.go
+++ src/app_test.go
@@ -1,8 +1,24 @@
 package src_test
 
-import "testing"
+import (
+	"github.com/axetroy/go-server/src"
+	"github.com/axetroy/go-server/src/controller/admin"
+	"github.com/axetroy/go-server/src/model"
+	"github.com/axetroy/go-server/src/service"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestInit(t *testing.T) {
-	// TODO: 测试初始化
-	t.Skip()
+	src.Init()
+
+	// 删除管理员
+	defer admin.DeleteAdminByAccount("admin")
+
+	// 获取管理员
+	adminInfo := model.Admin{
+		Username: "admin",
+	}
+
+	assert.Nil(t, service.Db.Where(&adminInfo).First(&adminInfo).Error)
 }
