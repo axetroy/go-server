f7332c03f6900cdef7f796f915c7dc70d5d239ec
axetroy <troy450409405@gmail.com>
2019-05-29 17:15:46 +0800

    refactor router

--- src/router_user.go
+++ src/router_user.go
@@ -168,23 +168,20 @@ func init() {
 			v1.POST("/email/send/activation", email.SendActivationEmailRouter)        // 发送激活邮件
 			v1.POST("/email/send/password/reset", email.SendResetPasswordEmailRouter) // 发送密码重置邮件
 
-			// 文件上传 (需要验证token)
-			uploadRouter := v1.Group("/upload")
-			{
-				uploadRouter.POST("/file", uploader.File)      // 上传文件
-				uploadRouter.POST("/image", uploader.Image)    // 上传图片
-				uploadRouter.GET("/example", uploader.Example) // 上传文件的 example
-			}
+			// 文件上传
+			v1.POST("/upload/file", uploader.File)      // 上传文件
+			v1.POST("/upload/image", uploader.Image)    // 上传图片
+			v1.GET("/upload/example", uploader.Example) // 上传文件的 example
 			// 单纯获取资源文本
-			v1.GET("/resource/file/:filename", resource.File)
-			v1.GET("/resource/image/:filename", resource.Image)
-			v1.GET("/resource/thumbnail/:filename", resource.Thumbnail)
+			v1.GET("/resource/file/:filename", resource.File)           // 获取文件纯文本
+			v1.GET("/resource/image/:filename", resource.Image)         // 获取图片纯文本
+			v1.GET("/resource/thumbnail/:filename", resource.Thumbnail) // 获取缩略图纯文本
 			// 下载资源
-			v1.GET("/download/file/:filename", downloader.File)
-			v1.GET("/download/image/:filename", downloader.Image)
-			v1.GET("/download/thumbnail/:filename", downloader.Thumbnail)
+			v1.GET("/download/file/:filename", downloader.File)           // 下载文件
+			v1.GET("/download/image/:filename", downloader.Image)         // 下载图片
+			v1.GET("/download/thumbnail/:filename", downloader.Thumbnail) // 下载缩略图
 			// 公共资源目录
-			v1.GET("/public/:filename", static.Get)
+			v1.GET("/public/:filename", static.Get)           // 获取静态文件
 			v1.GET("/avatar/:filename", user.GetAvatarRouter) // 获取用户头像
 
 			v1.GET("/area", address.AreaListRouter) // 获取地址选择列表
