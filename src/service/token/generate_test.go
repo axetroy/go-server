ceb95d8549fd0e9c4547731ee2f723c63c330600
axetroy <troy450409405@gmail.com>
2019-05-29 14:37:28 +0800

    add test case for token

---
+++ src/service/token/generate_test.go
@@ -0,0 +1,21 @@
+package token_test
+
+import (
+	"github.com/axetroy/go-server/src/service/token"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	uid := "123123"
+	tokenStr, err := token.Generate(uid, false)
+
+	assert.Nil(t, err)
+	assert.IsType(t, "123", tokenStr)
+
+	c, err1 := token.Parse(token.Prefix+" "+tokenStr, false)
+
+	assert.Nil(t, err1)
+
+	assert.Equal(t, uid, c.Id)
+}
