4507b2cbef3360dbd837b77576a5beb2a8bc093d
axetroy <troy450409405@gmail.com>
2019-05-29 17:34:39 +0800

    fix test

--- src/util/password_test.go
+++ src/util/password_test.go
@@ -11,16 +11,10 @@ func TestGenerate(t *testing.T) {
 	s := util.GeneratePassword(testPassword)
 
 	// 生成的密码与预期的一致
-	if !assert.Equal(t, "39d9782aa70de6b4944c40991ac37004", s) {
-		return
-	}
+	assert.Equal(t, "c52f65639a16da778bd8839424495012", s)
 
 	// 生成两次的密码保持一致
-	if !assert.Equal(t, util.GeneratePassword(testPassword), util.GeneratePassword(testPassword)) {
-		return
-	}
+	assert.Equal(t, util.GeneratePassword(testPassword), util.GeneratePassword(testPassword))
 
-	if !assert.Len(t, util.GeneratePassword(testPassword), 32, "密码必须是32位") {
-		return
-	}
+	assert.Len(t, util.GeneratePassword(testPassword), 32, "密码必须是32位")
 }
