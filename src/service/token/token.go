d8d942c7a594538b60d31058d7ff0a85e900f031
axetroy <troy450409405@gmail.com>
2019-05-29 14:16:06 +0800

    JWT 的 Token 密钥可在 .env 中自行定义

--- src/service/token/token.go
+++ src/service/token/token.go
@@ -1,14 +1,19 @@
 package token
 
 import (
+	"errors"
 	"github.com/dgrijalva/jwt-go"
+	"os"
 )
 
 var (
 	userSecreteKey  = "user"
 	adminSecreteKey = "admin"
-	Prefix          = "Bearer"
-	AuthField       = "Authorization"
+)
+
+const (
+	Prefix    = "Bearer"
+	AuthField = "Authorization"
 )
 
 type Claims struct {
@@ -19,4 +24,21 @@ type Claims struct {
 type ClaimsInternal struct {
 	Uid string `json:"uid"` // base64 encode
 	jwt.StandardClaims
+}
+
+func init() {
+	userKey := os.Getenv("USER_TOKEN_SECRET_KEY")
+	adminKey := os.Getenv("ADMIN_TOKEN_SECRET_KEY")
+
+	if userKey != "" {
+		userSecreteKey = userKey
+	}
+
+	if adminKey != "" {
+		adminSecreteKey = adminKey
+	}
+
+	if userSecreteKey == adminSecreteKey {
+		panic(errors.New("用户端的 Token 密钥不能和管理员端的相同，存在安全风险"))
+	}
 }
