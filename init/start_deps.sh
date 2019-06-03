0b62b797213128542927fb3e28c8e914ca8aade3
axetroy <troy450409405@gmail.com>
2019-06-03 21:27:53 +0800

    添加启动脚本

---
+++ init/start_deps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose.mq.yml up -d
+docker-compose up -d
---
+++ init/stop_deps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker-compose -f docker-compose.mq.yml down
+docker-compose down
