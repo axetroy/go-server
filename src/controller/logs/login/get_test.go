de782daeac4d60239fc85c0df5df62e29c507ea8
axetroy <troy450409405@gmail.com>
2019-08-24 18:35:51 +0800

    test: ignore incomplete test case

--- src/controller/logs/login/get_test.go
+++ src/controller/logs/login/get_test.go
@@ -1,20 +1,15 @@
 package login_test
 
 import (
-	"fmt"
-	"github.com/axetroy/go-server/src/controller"
-	"github.com/axetroy/go-server/src/controller/auth"
-	"github.com/axetroy/go-server/src/controller/logs/login"
-	"github.com/axetroy/go-server/tester"
 	"testing"
 )
 
 func TestGetLoginLog(t *testing.T) {
-	user, _ := tester.CreateUser()
-
-	defer auth.DeleteUserByUserName(user.Username)
-
-	r := login.GetLatestLog(&controller.Context{Uid: user.Id})
-
-	fmt.Printf("%+v\n", r)
+	//user, _ := tester.CreateUser()
+	//
+	//defer auth.DeleteUserByUserName(user.Username)
+	//
+	//r := login.GetLatestLog(&controller.Context{Uid: user.Id})
+	//
+	//fmt.Printf("%+v\n", r)
 }
