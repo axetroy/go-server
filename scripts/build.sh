b8df304e10cceb5ba66e62a0c107e8f1c33e0869
Axetroy <axetroy.dev@gmail.com>
2019-09-27 11:04:42 +0800

    更新构建脚本

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,40 +2,9 @@
 
 # Reference:
 # https://github.com/golang/go/blob/master/src/go/build/syslist.go
-os_archs=(
-    darwin/386
-    darwin/amd64
-    dragonfly/amd64
-    freebsd/386
-    freebsd/amd64
-    freebsd/arm
-    linux/386
-    linux/amd64
-    linux/arm
-    linux/arm64
-    linux/ppc64
-    linux/ppc64le
-    linux/mips
-    linux/mipsle
-    linux/mips64
-    linux/mips64le
-    linux/s390x
-    nacl/386
-    nacl/amd64p32
-    nacl/arm
-    netbsd/386
-    netbsd/amd64
-    netbsd/arm
-    openbsd/386
-    openbsd/amd64
-    openbsd/arm
-    plan9/386
-    plan9/amd64
-    plan9/arm
-    solaris/amd64
-    windows/386
-    windows/amd64
-)
+os_archs=$(go tool dist list)
+
+os_archs=(${os_archs//$'\n'/ })
 
 releases=()
 targets=(
