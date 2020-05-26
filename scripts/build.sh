ac0abb0e1f328dbe7779e8013450aa20767caff7
艾斯特洛 <axetroy.dev@gmail.com>
2020-05-26 15:39:37 +0800

    chore: rebuild ci (#278)

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,51 +9,42 @@ os_archs=(
 )
 
 releases=()
-targets=(
-    user # 用户接口
-    admin # 管理员接口
-    resource # 资源服务器接口
-    message_queue # 消息队列
-)
+target=$1
 
-for target in "${targets[@]}"
+for os_arch in "${os_archs[@]}"
 do
-    for os_arch in "${os_archs[@]}"
-    do
-        goos=${os_arch%/*}
-        goarch=${os_arch#*/}
+    goos=${os_arch%/*}
+    goarch=${os_arch#*/}
 
-        filename=${target}_server
+    filename=${target}_server
 
-        if [[ ${goos} == "windows" ]];then
-            filename+=.exe
-        fi
+    if [[ ${goos} == "windows" ]];then
+        filename+=.exe
+    fi
 
-        echo building ${target} ${os_arch}
+    echo building ${target} ${os_arch}
 
-        CGO_ENABLED=0 GOOS=${goos} GOARCH=${goarch} go build -mod=vendor -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH -ldflags "-w -s" -o ./bin/${filename} ./cmd/${target}/main.go
+    CGO_ENABLED=0 GOOS=${goos} GOARCH=${goarch} go build -mod=vendor -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH -ldflags "-w -s" -o ./bin/${filename} ./cmd/${target}/main.go
 
-        # if build success
-        if [[ $? == 0 ]];then
-            releases+=(${os_arch})
-            cd ./bin
+    # if build success
+    if [[ $? == 0 ]];then
+        releases+=(${os_arch})
+        cd ./bin
 
-            tar -czf ${target}_server_${goos}_${goarch}.tar.gz ${filename}
+        tar -czf ${target}_server_${goos}_${goarch}.tar.gz ${filename}
 
-            rm -rf ./${filename}
+        rm -rf ./${filename}
 
-            cd ../
-        else
-          exit $?
-        fi
-    done
-
-    echo "${target} release:"
-
-    for os_arch in "${releases[@]}"
-    do
-        printf "\t%s\n" "${os_arch}"
-    done
-    echo
+        cd ../
+    else
+      exit $?
+    fi
 done
 
+echo "${target} release:"
+
+for os_arch in "${releases[@]}"
+do
+    printf "\t%s\n" "${os_arch}"
+done
+echo
